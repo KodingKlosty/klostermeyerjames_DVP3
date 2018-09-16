5b8322ceac1523fe4e184ae2ffa6226a464f5e45
James K <jdklostermeyer@student.fullsail.edu>
2018-09-16 11:41:21 -0400

    Updated first paragraph and background

--- week3/js/form.js
+++ week3/js/form.js
@@ -1,1 +1,13 @@
+// Update page title
 document.querySelector("h2").innerHTML = "<h2><strong>Destiny 2</strong> Contact Form</h2>";
+
+// Change backgroud image
+document.body.style.background = "#222222 url('https://kodingklosty.github.io/klostermeyerjames_DVP3/week3/images/FormBGimage.jpg')";
+
+// Update First Paragraph
+var firstP = "We are sorry you are having issues guardian. Please enter you infromation and submit it to the postmaster."
+var getfirst = document.querySelector("p");
+getfirst.textContent = firstP;
+
+
+

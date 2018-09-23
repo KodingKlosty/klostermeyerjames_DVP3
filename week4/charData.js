026f180041d3ede39dc7607e7c2c0ba814e41133
James K <jdklostermeyer@student.fullsail.edu>
2018-09-23 09:22:10 -0400

    fetch change

--- week4/charData.js
+++ week4/charData.js
@@ -8,11 +8,18 @@ http://destinydevs.github.io/BungieNetPlatform/docs/Getting-Started
 
 const apiKey = "ed9eb60a012f45588c35e5028467e0c6";
 const D2Manifest = "https//www.bungie.net/platform/Destiny2/Manifest/"
-const json = await Response.json();
+
 
 
 fetch(D2Manifest)
-console.log(json);
+    .then(function(response)
+    {
+        return response.json();
+    })
+    .then(function(d2json)
+    {
+        console.log(JSON.stringify(d2json));
+    });
 /*var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://www.bungie.net/platform/Destiny2/Manifest/", true);
 xhr.setRequestHeader("X-API-Key", apiKey);
